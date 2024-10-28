e3b9f5d0f1eeff34c7c9c15c6a456ec922061ccf
Alexander CS Hendorf <hendorf@gmail.com>
2024-10-28 19:13:31 +0100

    added on site ops to guidelines, updated logo placement

---
+++ docs/guidelines/on-site-ops.md
@@ -0,0 +1,10 @@
+---
+title: On Site Ops Guidelines
+icon: fontawesome/solid/people-group
+---
+# On-Site Operations 
+
+!!! warning
+    This document is a work in progress. Please check back later for updates.
+
+---
--- scripts/hook_add_logo_to_social_cards.py
+++ scripts/hook_add_logo_to_social_cards.py
@@ -14,18 +14,22 @@ def on_post_build(config):
     # config extra as no attributes, workaround
     xtra = dict(config.extra)
     path_to_logo = root / xtra["social_cards"]["logo_path"]
-    the_logo_position = (960, 450)
     base_width = 175
+    the_logo_position = (1200 - base_width - 50, 630 - base_width - 50)
     ph_logo = Image.open(path_to_logo)
-    w_percent = (base_width / ph_logo.size[0])
-    h_size = int(float(ph_logo.size[1]) * float(w_percent))
-    ph_logo = ph_logo.resize((base_width, h_size), Image.Resampling.LANCZOS)
+    if ph_logo.size[0] != base_width:
+        w_percent = (base_width / ph_logo.size[0])
+        h_size = int(float(ph_logo.size[1]) * float(w_percent))
+        ph_logo = ph_logo.resize((base_width, h_size), Image.Resampling.LANCZOS)
     for k, dir_from_project_root in xtra["social_cards"]["dirs"].items():
         read_dir = root / dir_from_project_root
         for social_card in read_dir.glob('*.png'):
             # Get the social card image
             social_card_image = Image.open(social_card)
             # Paste the Pioneers Hub logo onto the social card image
-            social_card_image.paste(ph_logo, the_logo_position, mask=ph_logo)
+            try:
+                social_card_image.paste(ph_logo, the_logo_position, mask=ph_logo)
+            except ValueError:
+                social_card_image.paste(ph_logo, the_logo_position)
             # Save the new social card image
             social_card_image.save(social_card, quality=95)
